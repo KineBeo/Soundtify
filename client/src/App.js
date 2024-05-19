f9096003ebe717e16b02515204d85919c0b93364
Kinebeo <kienbeovl1392004@gmail.com>
2024-05-19 13:10:58 +0700

    feat: create authentication component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
+import { Authentication } from "./components/Authentication"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-blue-900">                                                                                    
-      Hello world!                                              
-    </h1>
+   <div> 
+      <Authentication/>
+   </div>
   )
 }                                                                                                                                                                                                                                                                                   
