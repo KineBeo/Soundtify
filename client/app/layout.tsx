8c9d3c9a0f69fa56e49c65d39ecc2f43a46621e8
Kinebeo <kienbeovl1392004@gmail.com>
2024-07-08 11:05:11 +0700

    feat: add audioHandler component to play song with click

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import StoreProvider from "./StoreProvider";
+import AudioHandler from "@/components/AudioHandler";
 const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,6 +23,7 @@ export default function RootLayout({
           <Sidebar>
             {children}
           </Sidebar>
+          <AudioHandler />
         </StoreProvider>
       </body>
     </html>
---
+++ client/components/AudioHandler.tsx
@@ -0,0 +1,74 @@
+'use client'
+import {
+    AudioPlayerState,
+    playPause
+} from "@/lib/features/audioPlayer/audioPlayerSlice";
+import { useAppSelector, useAppDispatch } from "@/lib/hook";
+import { RootState } from "@/lib/store";
+import { Howl } from "howler";
+import { useRef, useEffect } from "react";
+function AudioHandler() {
+
+    const {
+        isPlaying,
+        activeSong,
+        currentIndex,
+        seekTime,
+        currentTime,
+        volume,
+        fetchLikedStatus,
+        playlistStatus,
+        queue: songs,
+        isShuffle,
+        isRepeat,
+    }: AudioPlayerState = useAppSelector((state: RootState) => state.audioPlayer);
+
+    const dispatch = useAppDispatch();
+    const audioRef = useRef<Howl | null>(null);
+    const isReady = useRef(false);
+    useEffect(() => {
+        if (isPlaying) {
+            if (audioRef.current) {
+                audioRef.current.play();
+            }
+        } else {
+            if (audioRef.current) {
+                audioRef.current.pause();
+            }
+        }
+    }, [isPlaying]);
+
+    useEffect(() => {
+        if (!activeSong) {
+            return;
+        }
+
+        if (audioRef.current) {
+            audioRef.current.pause();
+        }
+
+        audioRef.current = new Howl({
+            src: activeSong?.src ? [activeSong.src] : [],
+            html5: true,
+            // onplay: handlePlay,
+            // onpause: handlePause,
+        });
+
+        if (isReady.current) {
+            audioRef.current.play();
+            dispatch(playPause(true));
+        } else {
+            isReady.current = true;
+        }
+
+        return () => {
+            if (audioRef.current) {
+                audioRef.current.pause();
+            }
+        }
+    }, [activeSong, currentIndex]);
+
+    return <></>
+}
+
+export default AudioHandler;
