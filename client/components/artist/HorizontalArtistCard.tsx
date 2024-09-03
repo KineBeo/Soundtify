4b9a89046f8f750fb92c646dab9ded4d28ddf859
kienbeovl <kienbeovl@Macbook-Pro-cua-Kinebeo.local>
2024-09-03 23:40:29 +0700

    fix: refactor horizontal artist list show method

--- client/components/artist/HorizontalArtistCard.tsx
+++ client/components/artist/HorizontalArtistCard.tsx
@@ -3,72 +3,58 @@ import Artist from "@/interfaces/artist";
 import Image from "next/image";
 
 interface HorizontalArtistCardProps {
-    artist: Artist;
-    onClick: () => void;
+  artist: Artist;
+  onClick: () => void;
 }
 
 const HorizontalArtistCard: React.FC<HorizontalArtistCardProps> = ({
-    artist,
-    onClick,
+  artist,
+  onClick,
 }) => {
-    return (
+  return (
+    <div
+      className="cursor-pointer"
+      onClick={onClick}
+    >
+      <div
+        className="
+          p-4 bg-gradient-to-t from-[#2c2a2a4a] to-[#2c2a2ac7] hover:bg-[#4340409d]
+          tablet:hover:bg-transparent mobile:hover:bg-transparent
+          rounded h-full mini-laptop:p-3 tablet:p-0 tablet:from-transparent tablet:to-transparent
+          mobile:from-transparent mobile:to-transparent mobile:p-0 transition-all
+        "
+      >
         <div
-            className="
-            relative
-            group
-            flex
-            flex-col
-            items-center
-            justify-center
-            rounded-md
-            overflow-hidden
-            gap-x-4
-            bg-neutral-400/5
-            cursor-pointer
-            hover:bg-neutral-400/20
-            transition
-            p-3"
-            onClick={onClick}
+          className="
+            w-[160px] h-[160px] relative rounded-full overflow-hidden
+            mini-laptop:w-[140px] mini-laptop:h-[140px] 
+            tablet:w-[130px] tablet:h-[130px] mobile:w-[100px] mobile:h-[100px]
+          "
         >
-            <div
-                className="
-                relative
-                aspect-square
-                w-full
-                h-full
-                rounded-md
-                overflow-hidden
-                "
-            >
-
-                <Image
-                    src={artist.avatar.url}
-                    alt="artist cover image"
-                    width={200}
-                    height={200}
-                    className="w-full h-full rounded-full object-cover"
-                    loading="lazy"
-                />
-            </div>
-            <div
-                className="flex flex-col items-start w-full pt-4 gap-y-1"
-            >
-                <p className="font-semibold truncate w-full">
-                    {artist.display_name}
-                </p>
-                <p className="
-                text-neutral-400
-                text-md
-                pb-4
-                w-full
-                truncate
-                ">
-                    Artist
-                </p>
-
-            </div>
+          <Image
+            src={artist.avatar.url}
+            alt={`${artist.display_name} cover image`}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+          />
         </div>
-    )
-}
 
-export default HorizontalArtistCard
+        <p className="line-clamp-2 mobile:text-center tablet:text-center mt-4 text-base mobile:text-sm tablet:text-sm">
+          {artist.display_name}
+        </p>
+        <p
+          className="
+            line-clamp-2 mt-0.5 text-sm text-gray-400 
+            mobile:text-xs tablet:text-xs
+            mobile:text-center tablet:text-center
+          "
+        >
+          Artist
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HorizontalArtistCard;
--- client/components/artist/HorizontalArtistList.tsx
+++ client/components/artist/HorizontalArtistList.tsx
@@ -1,41 +1,34 @@
-'use client'
-import Artist from '@/interfaces/artist'
-import React from 'react'
-import HorizontalArtistCard from './HorizontalArtistCard';
-import { useRouter } from 'next/navigation';
-
+"use client";
+import Artist from "@/interfaces/artist";
+import React from "react";
+import HorizontalArtistCard from "./HorizontalArtistCard";
+import { useRouter } from "next/navigation";
+import ScrollContainer from "react-indiana-drag-scroll";
 interface HorizontalArtistListProps {
-    artists: Artist[];
+  artists: Artist[];
 }
 
 const HorizontalArtistList: React.FC<HorizontalArtistListProps> = ({
-    artists
+  artists,
 }) => {
+  const router = useRouter();
+  return (
+    <ScrollContainer
+      horizontal={true}
+      vertical={false}
+      className="flex flex-row gap-4 mt-4"
+    >
+      {artists.map((artist: Artist) => (
+        <HorizontalArtistCard
+          key={artist.id}
+          artist={artist}
+          onClick={() => {
+            router.push(`/artist/${artist.id}`);
+          }}
+        />
+      ))}
+    </ScrollContainer>
+  );
+};
 
-    const router = useRouter();
-    return (
-        <div
-            className='
-        grid
-        mobile:grid-cols-2
-        tablet:grid-cols-3
-        mini-laptop:grid-cols-3
-        laptop:grid-cols-5
-        desktop:grid-cols-8
-        gap-4
-        mt-4'>
-
-            {artists.map((artist: Artist) => (
-                <HorizontalArtistCard
-                    key={artist.id}
-                    artist={artist}
-                    onClick={() => {
-                        router.push(`/artist/${artist.id}`);
-                    }}
-                />
-            ))}
-        </div>
-    )
-}
-
-export default HorizontalArtistList
+export default HorizontalArtistList;
